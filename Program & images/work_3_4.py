d8413518d0c1954bdb31a1fa7b9e725324c1f0f0
nived-s <nivedsreenilayam@gmail.com>
2023-04-30 18:26:32 +0530

    contour detection

---
+++ Program & images/work_3_4.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('detect_blob.png', 1)
+
+gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 155, 1)
+
+cv2.imshow("Binary", thresh)
+
+contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+img2 = img.copy()
+
+index = -1
+thickness = 4
+color = (255, 0, 255)
+
+cv2.drawContours(img2, contours, index, color, thickness)
+
+cv2.imshow("contours", img2)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
